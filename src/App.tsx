3fbd828703c1f2c19ab209372af122b43087b49b
Sava1608 <vtoper43@gmail.com>
2024-05-15 11:34:28 +0300

    init HomeWork5

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import PostComponent from "./componentss/PostComponent";
 
 const App = () => {
   return (
       <div>
-        
+        <PostComponent/>
       </div>
   );
 };
--- src/componentss/PostComponent.tsx
+++ src/componentss/PostComponent.tsx
@@ -1,10 +1,21 @@
 import React, {FC} from 'react';
 
-
+interface IPostProps{
+    title:string;
+    body:string;
+}
 const FormComponent:FC = () => {
+
+
+
     return (
         <div>
-
+            <form>
+                <input type="text"/>
+                <input type="text"/>
+                <input type="number"/>
+                <button>Click me</button>
+            </form>
         </div>
     );
 };
