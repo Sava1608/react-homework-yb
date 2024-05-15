28405cc51a6b380763f4754f8b9c1a7deb90793d
Sava1608 <vtoper43@gmail.com>
2024-05-15 16:18:27 +0300

    init HomeWork5

--- src/componentss/PostComponent.tsx
+++ src/componentss/PostComponent.tsx
@@ -4,23 +4,46 @@ import {joiResolver} from "@hookform/resolvers/joi";
 import {postValidator} from "../validators/post.validator";
 
 interface IPostProps{
+    id:number;
     title:string;
     body:string;
     userId:number;
 }
 const FormComponent:FC = () => {
-let {register,handleSubmit} = useForm<IPostProps>({mode:"all", resolver:joiResolver(postValidator)});
+let {register,
+    handleSubmit,
+    formState:{errors,}
+} = useForm<IPostProps>({mode:"all", resolver: joiResolver(postValidator)});
 
 
-const save = (postsObj:IPostProps) =>{
-    console.log(postsObj)
+const save = ({title,body,userId}:IPostProps) =>{
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: title,
+            body: body,
+            userId: userId,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+
 }
     return (
         <div>
             <form onSubmit={handleSubmit(save)}>
                 <input type="text" {...register('title')}/>
+                {errors.title && <span>{errors.title.message}</span>}
+                <br/>
                 <input type="text" {...register('body')}/>
+                {errors.body && <span>{errors.body.message}</span>}
+                <br/>
                 <input type="number" {...register('userId')}/>
+                {errors.userId && <span>{errors.userId.message}</span>}
+                <br/>
                 <button>Click me</button>
             </form>
         </div>
--- src/validators/post.validator.ts
+++ src/validators/post.validator.ts
@@ -1,3 +1,7 @@
 import Joi from "joi";
 
-export const postValidator = Joi
+export const postValidator = Joi.object({
+    title: Joi.string().required().messages({'string.required': 'Error =('}),
+    body: Joi.string().required().messages({'string.required': 'Error =(('}),
+    userId: Joi.number().min(1).max(10).required().messages({'number.required': 'Error number =((('})
+})
