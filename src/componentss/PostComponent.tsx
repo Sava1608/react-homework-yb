88055c342782516f4d3bac05a181c0d997c75b20
Sava1608 <vtoper43@gmail.com>
2024-05-15 14:36:52 +0300

    init HomeWork5

--- src/componentss/PostComponent.tsx
+++ src/componentss/PostComponent.tsx
@@ -1,19 +1,26 @@
 import React, {FC} from 'react';
+import {useForm} from "react-hook-form";
+import {joiResolver} from "@hookform/resolvers/joi";
+import {postValidator} from "../validators/post.validator";
 
 interface IPostProps{
     title:string;
     body:string;
+    userId:number;
 }
 const FormComponent:FC = () => {
+let {register,handleSubmit} = useForm<IPostProps>({mode:"all", resolver:joiResolver(postValidator)});
 
 
-
+const save = (postsObj:IPostProps) =>{
+    console.log(postsObj)
+}
     return (
         <div>
-            <form>
-                <input type="text"/>
-                <input type="text"/>
-                <input type="number"/>
+            <form onSubmit={handleSubmit(save)}>
+                <input type="text" {...register('title')}/>
+                <input type="text" {...register('body')}/>
+                <input type="number" {...register('userId')}/>
                 <button>Click me</button>
             </form>
         </div>
